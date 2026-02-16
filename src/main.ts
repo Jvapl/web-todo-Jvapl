b22c45961c42658c2e9fd6b6adae552cf9d57f45
joao aebi <joao.aebi@jobtrek.ch>
2026-02-16 16:01:23 +0100

    feat: add simple CSS style and started to add lists for todos closes #2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,31 @@
 import './style.css'
+const inputTodo = document.querySelector('#todo-input') as HTMLInputElement
+const buttonAdd = document.querySelector(
+  '#add-todo-button',
+) as HTMLButtonElement
+const todoElements = document.querySelector(
+  '#todo-elements',
+) as HTMLUListElement
+const errorInput = document.querySelector('#error') as HTMLParagraphElement
 
-console.log('Hello from typescript')
+const addElement = () => {
+  const text = inputTodo.value.trim()
+  if (!text) {
+    errorInput.removeAttribute('hidden')
+    return
+  }
+  errorInput.setAttribute('hidden', '')
+  const newLi = document.createElement('li')
+  newLi.textContent = text
+  todoElements.appendChild(newLi)
+  inputTodo.value = ''
+}
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addElement()
+  }
+})
+buttonAdd.addEventListener('click', () => {
+  addElement()
+})
