a2a34ad746ce99937b3f585cb065366ec350ed25
joao aebi <joao.aebi@jobtrek.ch>
2026-02-17 14:55:58 +0100

    chore: stockage of todos in local storage closes #3

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,47 @@ const buttonAdd = document.querySelector<HTMLButtonElement>('#add-todo-button')
 const todoElements = document.querySelector<HTMLUListElement>('#todo-elements')
 const errorInput = document.querySelector<HTMLParagraphElement>('#error')
 
+interface TaskType {
+  id: number
+  name: string
+}
+const taskTodo: TaskType[] = []
+
 if (!inputTodo || !buttonAdd || !todoElements || !errorInput) {
   throw new Error(
-    'Un ou plusieurs éléments DOM sont introuvables. Vérifiez les IDs dans index.html.', //many DOM elements weren't
+    "Didn't find one or many DOM elements. Verify the IDs from index.html.", //
   )
 }
+
+const displayTask = (text: TaskType) => {
+  const newLi = document.createElement('li')
+  newLi.textContent = text.name
+  todoElements.appendChild(newLi)
+}
+
+const reload = localStorage.getItem('taskTodo')
+if (reload) {
+  const savedTasks = JSON.parse(reload)
+  taskTodo.push(...savedTasks)
+
+  taskTodo.forEach((taskText) => {
+    displayTask(taskText)
+  })
+}
+
 const addElement = () => {
-  const text = inputTodo.value.trim()
+  const text: string = inputTodo.value.trim()
   if (!text) {
     errorInput.removeAttribute('hidden')
     return
   }
+  const newTask: TaskType = { id: Date.now(), name: text }
   errorInput.setAttribute('hidden', '')
-  const newLi = document.createElement('li')
-  newLi.textContent = text
-  todoElements.appendChild(newLi)
+  displayTask(newTask)
+  taskTodo.push(newTask)
   inputTodo.value = ''
+  const JSONFication = JSON.stringify(taskTodo)
+  localStorage.setItem('taskTodo', JSONFication)
 }
 
 inputTodo.addEventListener('keydown', (event) => {
